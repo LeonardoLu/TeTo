a4edbb19406bd6e61d40b41d53209b157691e75b
陆俊辉 <lujunhui.2nd@bytedance.com>
2022-07-17 13:23:33 +0800

    feat: init PB & client

---
+++ client/TeTo/Shared/TeToApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeToApp.swift
+//  Shared
+//
+//  Created by lujunhui.2nd on 2022/7/17.
+//
+
+import SwiftUI
+
+@main
+struct TeToApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
